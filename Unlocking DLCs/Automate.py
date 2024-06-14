54ab342f1cf89ef915042bd11ef4460710cef843
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-06-14 14:21:15 +0300

    feat: Hallelujah thank You Jesus Christ our Holy Lord GOD Almighty

--- Unlocking DLCs/Automate.py
+++ Unlocking DLCs/Automate.py
@@ -34,18 +34,36 @@ New_File_Name: str = "File.dll"
 New_File_Path: str = os.path.join(Current_Directory, New_File_Name)
 Old_File_Name: str = "EOSSDK-Win64-Shipping.dll"
 
-Does_Old_File_Exist: bool = os.path.exists(os.path.join(Plugins_Folder, Old_File_Name))
-if not Does_Old_File_Exist:
-    console.print(f"Old file {Old_File_Name} not found", style="bold red")
-    exit()
+with console.status("[yellow]Looking for original file...[/yellow]"):
+    Does_Old_File_Exist: bool = os.path.exists(
+        os.path.join(Plugins_Folder, Old_File_Name)
+    )
+    if not Does_Old_File_Exist:
+        console.print(f"Found the original file {Old_File_Name}", style="bold red")
+        exit()
+    else:
+        console.print(f"Didnt find original file {Old_File_Name}", style="bold green")
 
-os.rename(
-    os.path.join(Plugins_Folder, Old_File_Name),
-    os.path.join(Plugins_Folder, Old_File_Name + "_old"),
-)
-shutil.copy2(New_File_Path, Plugins_Folder)
-New_File_Path: str = os.path.join(Plugins_Folder, New_File_Name)
-os.rename(
-    New_File_Path,
-    os.path.join(Plugins_Folder, Old_File_Name),
-)
+with console.status("[yellow]Renaming the file...[/yellow]"):
+    try:
+        File_Name: str = Old_File_Name.replace(".dll", "")
+        os.rename(
+            os.path.join(Plugins_Folder, Old_File_Name),
+            os.path.join(Plugins_Folder, File_Name + "_old.dll"),
+        )
+        console.print(
+            f"Renamed {Old_File_Name} to {File_Name}_old.dll", style="bold green"
+        )
+    except:
+        console.print(f"Failed to rename {Old_File_Name}", style="bold red")
+
+with console.status("[yellow]Copying and renaming the new file...[/yellow]"):
+    try:
+        shutil.copy2(New_File_Path, Plugins_Folder)
+        os.rename(
+            os.path.join(Plugins_Folder, New_File_Name),
+            os.path.join(Plugins_Folder, Old_File_Name),
+        )
+        console.print(f"Copied {New_File_Name} to {Old_File_Name}", style="bold green")
+    except:
+        console.print(f"Failed to copy {New_File_Name}", style="bold red")
